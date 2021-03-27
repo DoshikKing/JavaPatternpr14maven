a60be35a18b44d121dc3042eee04994bdaf166a4
Admin <qwertyuiop12345jomh@gmail.com>
2021-03-27 16:53:37 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/CardController.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import com.example.demo.components.Bank;
+import com.example.demo.components.Card;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/home")
+public class CardController {
+    @Autowired
+    private BankWork bankWork;
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/show")
+    public @ResponseBody String show(){
+        String buff = "<h>Banks</h><table border=1>";
+        int i = 0;
+        for (Bank item:bankWork.getBanks()) {
+            buff += "<tr><td>" + item.getName() + "</td><td>" + item.getAddress() + "</td><td><a href='removeBank?id=" + i + "'>Delete</a></td></tr>";
+            i++;
+        }
+        if (i == 0)
+        {
+            buff+= "<tr><td>Bank list is empty</td></tr>";
+        }
+        i = 0;
+        buff += "</table>";
+        buff += "<h>Cards</h><table border=1>";
+        for (Card item:bankWork.getCards()) {
+            buff += "<tr><td>" + item.getCode() + "</td><td>" + item.getCardNumber() + "</td><td><a href='removeCard?id=" + i + "'>Delete</a></td></tr>";
+            i++;
+        }
+        if (i == 0)
+        {
+            buff+= "<tr><td>Card list is empty</td></tr>";
+        }
+        buff += "</table>";
+        return buff;
+    }
+
+    @PostMapping("/addBank")
+    public String add(@RequestParam String name,
+                      @RequestParam String address){
+        Bank bank = new Bank();
+        bank.setName(name);
+        bank.setAddress(address);
+        bankWork.saveBank(bank);
+        return "home";
+    }
+    @PostMapping("/addCard")
+    public String add(@RequestParam int cardNumber,
+                      @RequestParam int code){
+        Card card = new Card();
+        card.setCardNumber(cardNumber);
+        card.setCode(code);
+        bankWork.saveCard(card);
+        return "home";
+    }
+    @GetMapping("/removeCard")
+    public @ResponseBody String removeCards(@RequestParam int id){
+        bankWork.removeCards(id);
+        return show();
+    }
+    @GetMapping("/removeBank")
+    public @ResponseBody String removeBanks(@RequestParam int id){
+        bankWork.removeBanks(id);
+        return show();
+    }
+
+}
